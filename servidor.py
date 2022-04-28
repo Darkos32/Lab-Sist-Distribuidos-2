45f5c72adb0d87dbd7a851b972d358488b2f5fac
Jorge Oliveira <jorgeoliv32@gmail.com>
2022-04-27 22:11:31 -0300

    fim v1

--- servidor.py
+++ servidor.py
@@ -1,18 +1,47 @@
-from logging import exception
 from socket import *
 
+# Define a porta e o ip
 HOST = ''
 PORT = 5000
 
-
+#camada de acesso aos dados
 def acesso_dados(nome_arquivo):
     try:
-        arquivo = open(nome_arquivo, 'r')
+        arquivo = open(nome_arquivo, 'r')  # abre o arquivo pedido
     except:
+        # se o arquivo não existe lança uma exception
         raise RuntimeError("Arquivo não existente")
-    return arquivo.read()
+    conteudo = arquivo.read()
+    arquivo.close()  # fecha o arquivo
+    return conteudo
+
+# Encontra as 5 palavras mais recorrentes no dicionário
+def mais_recorrente(dicio):
+    top5 = [None, None, None, None, None]  # O vetor começa vazio
+    for palavra in dicio:
+        for i in range(0, len(top5)):
+            if top5[i] == None:  # se a posição está vazia insere o elemento
+                top5[i] = palavra
+                break
+            # se a palavra for mais recorrente que a da posição observada insere
+            elif dicio.get(palavra) > dicio.get(top5[i]):
+                top5 = insere(palavra, top5, i)
+                break
+    return top5
+
+#substitui os símbolos no intervalo passado por espaço em branco
+def replace_simbols(conteudo,inicio,fim):
+    for i  in range(inicio,fim):
+        conteudo = conteudo.replace(chr(i)," ")
+
+#elimina simbolos não considerados palavras
+def parse_conteudo(conteudo):
+    conteudo = replace_simbols(conteudo,33,48)
+    conteudo = replace_simbols(conteudo,58,65)
+    conteudo = replace_simbols(conteudo,91,97)
 
 
+#Camada de processamento
 def processamento(nome_arquivo):
     try:
         conteudo = acesso_dados(nome_arquivo)
@@ -22,9 +51,9 @@ def processamento(nome_arquivo):
         return resposta
     palavras = conteudo.split()
     dicio = contar(palavras)
-    mais_recorrente = mais_recorrente(dicio)
+    return mais_recorrente(dicio)
 
-
+#Calcula a quantidade de vezes que uma palavra aparece no arquivo
 def contar(palavras):
     dicio = {}
     for palavra in palavras:
@@ -34,10 +63,9 @@ def contar(palavras):
             dicio[palavra] = 0
     return dicio
 
-
+#Insere um elemento numa posição e "empurra" os elementos posteriores
 def insere(elemento, vetor, pos):
     temp = []
-
     for i in range(0, len(vetor)):
         if i == pos:
             temp.append(elemento)
@@ -46,27 +74,13 @@ def insere(elemento, vetor, pos):
     return temp
 
 
-def mais_recorrente(dicio):
-    top5 = [None, None, None, None, None]
-    for palavra in dicio:
-        for i in range(0, len(dicio)):
-            if top5[i] == None:
-                top5[i] = palavra
-                break
-            elif dicio.get(palavra) > dicio.get(top5[i]):
-                top5 = insere(palavra, top5, i)
-                break
-    return top5
-
-
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 while True:
-    (connect, add) = s.accept()
-    pedido = connect.recv(1024)
+    (connect, ende) = s.accept()
+    pedido = connect.recv(1024).decode("utf-8")
     resposta = processamento(str(pedido))
-    connect.send(str.encode( resposta))
-    #connect.recv()
-    s.close()
-
+    print(resposta)
+    connect.send(str.encode(str(resposta)))
+s.close()
